adae3d60f9b4c5dda86078af2053290f3391e588
Bjoern Kerler <info@revskills.de>
2022-08-06 17:09:56 +0200

    Fix libiio

--- patch_antsdr.sh
+++ patch_antsdr.sh
@@ -2,7 +2,10 @@
 cd plutosdr-fw
 git apply -p1 < ../patches/antsdr_main.patch
 cd buildroot
+git apply -p1 < ../../patches/libiio.patch
+rm package/libiio/0001-C-public-fields-with-getters-setters-cannot-be-marked-readonly.patch
 cp -r ../../patches/antsdr/buildroot/* .
+rm -rf package/gnuradio
 cp -r ../../patches/gr310/buildroot/* .
 cd ../hdl
 git apply -p1 < ../../patches/hdl.patch
