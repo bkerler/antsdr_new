25b33423bbb115b8027174ec8dd3094eae6cadf2
Bjoern Kerler <info@revskills.de>
2022-08-06 16:41:48 +0200

    Additional fixes

--- patch_antsdr.sh
+++ patch_antsdr.sh
@@ -9,9 +9,7 @@ cd ../hdl
 git apply -p1 < ../../patches/hdl.patch
 cd ../linux
 git apply -p1 < ../../patches/linux.patch
-git apply -p1 < ../../patches/gcc11.patch
 cd ../u-boot-xlnx/
 git apply -p1 < ../../patches/u-boot-xlnx.patch
-git apply -p1 < ../../patches/fix_arch_u-boot.patch
 cd ..
 echo "All patches are done. Plutosdr-fw is now Antsdr-fw :)"
